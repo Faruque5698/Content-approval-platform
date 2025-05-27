56dd543b4dfd9049f3c35fcd6fb5c0d3fc4dab0f
Faruque5698 <Ashaduzzaman5698@gmail.com>
2025-05-28 00:21:27 +0600

    cache data handle

--- app/Helpers/Functions/helpers.php
+++ app/Helpers/Functions/helpers.php
@@ -1,9 +1,20 @@
 <?php
 
+use Illuminate\Support\Facades\Cache;
+
 if ( !function_exists('generateUniqueCacheKey') ){
     function generateUniqueCacheKey(array $data, $perPage)
     {
         ksort($data);
         return 'users:' . md5(json_encode($data) . "_perPage_$perPage");
+    }
+}
+
+if ( !function_exists('invalidateCache') ){
+    function invalidateCache($model)
+    {
+        $cacheKey = strtolower(class_basename($model)) . '_cache_version';
+
+        Cache::increment($cacheKey);
     }
 }
